fe6d83eea9be4889ab3d5ee7736b2ff65efe2f2a
zane <zane.williamson@gmail.com>
2017-11-15 22:41:55 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # fib-sequence-generator
 Web interface for generating Fibonacci number sequences
+
+Todo:
+- Screen shots
+
+### Deployment
+
+Todo:
+- Dockerfile
+
+### Testing
+
+Todo:
+- unittest  
+- loop into Travis.ci
+
+### Local install
+
+```
+brew install python3
+pip3 install virtualenv
+cd fib-sequence-generator
+virtualenv -p python3 venv
+source venv/bin/activate
+```
+
+To deactivate
+
+```
+cd fib-sequence-generator
+deactivate
+```
+
+### Requirements
+
+* Python3
---
+++ fib-sequence-generator.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import make_response
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index_page():
+    return render_template('index.html')
+
+@app.route('/fib-generator', methods=['GET'])
+def fib_generator_page():
+    error = None
+    fib_max_number = request.args.get('fib-max-number')
+
+    try:
+        fib_max_number = int(request.args.get('fib-max-number'))
+    except ValueError as e:
+        return render_template('error_page.html', error=e)
+
+    if fib_max_number > 0:
+        fib_list = [n for n in _fibonacci(fib_max_number)]
+        return render_template('fib_generator.html',
+                               error=error, fib_sequence=fib_list)
+    else:
+        error = "Must provide a number greater than 0"
+        response = make_response(
+            render_template('error_page.html', error=error), 404)
+        return response
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error_page.html'), 404
+
+def _fibonacci(fib_max_number):
+    x, y = 0, 1
+    while x < fib_max_number:
+        yield x
+        x, y = y, x + y
