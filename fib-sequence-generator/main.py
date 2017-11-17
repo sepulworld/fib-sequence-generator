5a631754ffbb4f8b7b41e3935c97a131b9bfc34b
zane <zane.williamson@gmail.com>
2017-11-17 12:09:04 -0800

    Adding error and info logging

--- fib-sequence-generator/main.py
+++ fib-sequence-generator/main.py
@@ -13,19 +13,24 @@ def fib_generator_page():
 
     try:
         fib_max_number = int(request.args.get('fib-max-number'))
-    except ValueError:
+        app.logger.info("Max number to calculate fib seq to: {}"
+                        .format(fib_max_number))
+    except ValueError as e:
+        app.logger.error("Failure to set fib_max_number as int: {}".format(e))
         return render_template(
             'error_page.html',
             error="Not an integer! '{}'".format(fib_max_number))
 
     if fib_max_number > 0:
         fib_list = [n for n in _fibonacci(fib_max_number)]
+        app.logger.info("Fib list to be rendered {}".format(fib_list))
         return render_template('fib_generator.html',
                                error=error,
                                fib_sequence=fib_list,
                                fib_max_number=fib_max_number)
     else:
         error = "Must provide a number greater than 0"
+        app.logger.error("{}".format(error))
         response = make_response(
             render_template('error_page.html', error=error), 404)
         return response
