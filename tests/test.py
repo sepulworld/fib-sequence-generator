760c2a9da34fa23e92b77757991ec2b2fece848e
zane <zane.williamson@gmail.com>
2017-11-16 21:35:14 -0800

    Adding test, gitignore main.pyc

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+import unittest
+import sys
+sys.path.insert(0, '../app/')
+import main as fib_seq
+
+class TestFibSequence(unittest.TestCase):
+
+    def test_fib(self):
+        self.assertEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
+                         list(fib_seq._fibonacci(100)))
+
+if __name__ == '__main__':
+    unittest.main()
